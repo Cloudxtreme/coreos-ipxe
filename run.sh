11598244742e194edd92c401a6ed508adb676ed8
kciepluc <krzysztof.cieplucha@intel.com>
2016-04-24 22:23:28 +0000

    updated run.sh script

--- run.sh
+++ run.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-# re-create tftp directory if needed
-mkdir -p -m 755 /var/lib/tftp
-
-# fetch new versions of ipxe and coreos if needed
+echo fetching new versions of ipxe and coreos if needed
 cd /var/lib/tftp
-wget -c -N http://boot.ipxe.org/undionly.kpxe
-wget -c -N http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
-wget -c -N http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz.sig
-wget -c -N http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
-wget -c -N http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz.sig
+wget -c -N -nv http://boot.ipxe.org/undionly.kpxe
+wget -c -N -nv http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
+wget -c -N -nv http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz.sig
+wget -c -N -nv http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
+wget -c -N -nv http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz.sig
 
-# verify image signatures
+echo verifying image signatures
 gpg --trusted 50E0885593D2DCB4 --verify coreos_production_pxe.vmlinuz.sig
 gpg --trusted 50E0885593D2DCB4 --verify coreos_production_pxe_image.cpio.gz.sig
 
+echo starting dnsmasq
 # start dnsmasq
 exec dnsmasq --conf-file=/etc/dnsmasq.conf --no-daemon
 
